a32ae351d03ebcb3a64f0d22444d86b0e6bf0710
Aminah Statham-Adams <97044463+aminahSA@users.noreply.github.com>
2024-02-21 15:41:53 -0500

    create ui for mvp fragment

---
+++ app/src/main/Java/com/cs4520/assignment3/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.cs4520.assignment3
+
+import android.app.Activity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.Toast
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        }
+}

3adfabe92add31f8e0b0c35c8c04e1cabe9fcf9a
Aminah Statham-Adams <97044463+aminahSA@users.noreply.github.com>
2024-02-21 20:18:05 -0500

    implement navigation from home fragment to mvp fragment

--- app/src/main/Java/com/cs4520/assignment3/MainActivity.kt
+++ app/src/main/Java/com/cs4520/assignment3/MainActivity.kt
@@ -12,12 +12,23 @@ import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.Toast
-
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import com.cs4520.assignment3.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        // Get the NavHostFragment using its ID
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        // Get the NavController
+        val navController = navHostFragment.navController
+
+        // Navigate to the HomeFragment
+        navController.navigate(R.id.home_Fragment)
         }
 }
